02e03726d8f136040c91cd4794350ca2311b4d4d
Lucas Alves <hi.lucasalves@gmail.com>
2018-05-19 07:39:19 -0300

    Correção Produto

--- module/Application/src/Application/Form/LocalizarProdutoForm.php
+++ module/Application/src/Application/Form/LocalizarProdutoForm.php
@@ -49,7 +49,7 @@ class LocalizarProdutoForm extends AbstractForm {
             'name' => 'enviar',
             'attributes' => array(
                 'class' => 'btn btn-info',
-                'value' => 'Enviar'
+                'value' => 'Localizar'
             ),
         ));
     }
